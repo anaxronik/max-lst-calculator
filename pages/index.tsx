dee9a005a9de1ff0c7eca42571aee0bc2bd971e2
Sergey Komarov <anaxronik@gmail.com>
2021-12-14 21:37:02 +0500

    add es lint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import type { NextPage } from "next";
+import React from 'react';
+import type { NextPage } from 'next';
 
-const Home: NextPage = () => {
+const Home: NextPage = function () {
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
 
